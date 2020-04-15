00127652fa0207d883e75c5f3e05e08f9801fbe8
Karson Nichols <nicholsk18@gmail.com>
2020-04-15 07:29:45 -0500

    refactored action button

---
+++ src/main/java/com/karsonnichols/controller/userInterfaceActions/FindProjectPath.java
@@ -0,0 +1,105 @@
+package com.karsonnichols.controller.userInterfaceActions;
+
+import com.karsonnichols.model.CreateFilePath;
+import com.karsonnichols.model.ExcludePath;
+import com.karsonnichols.model.ReadFile;
+import com.karsonnichols.model.WriteToSource;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class FindProjectPath implements ActionListener {
+    private JFrame mainFrame;
+
+    private JTextArea inputSearchFor;
+    private JTextArea inputStringReplaceTo;
+    private JTextArea inputStringExclude;
+
+    private JLabel inputFileText;
+
+    private JCheckBox isLine;
+
+    public FindProjectPath (JFrame mainFrame, JTextArea inputSearchFor, JTextArea inputStringReplaceTo, JTextArea inputStringExclude, JLabel inputFileText, JCheckBox isLine) {
+        this.mainFrame = mainFrame;
+        this.inputSearchFor = inputSearchFor;
+        this.inputStringReplaceTo = inputStringReplaceTo;
+        this.inputStringExclude = inputStringExclude;
+        this.inputFileText = inputFileText;
+        this.isLine = isLine;
+    }
+
+    public void actionPerformed(ActionEvent event) {
+
+        // opens file window
+        JFileChooser fileChooser = new JFileChooser();
+        // allows for files and directories to be selected
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        int option = fileChooser.showOpenDialog(mainFrame);
+
+        // check to make sure right action was preformed
+        if (option == JFileChooser.APPROVE_OPTION) {
+            // String to hold file data
+            String afterLineRead = "";
+
+            // gets file and turns it to string path
+            File file = fileChooser.getSelectedFile();
+            String userPathToFile = file.getAbsolutePath();
+
+            // set the path
+            CreateFilePath filePath = new CreateFilePath(userPathToFile, inputStringExclude.getText());
+
+            ReadFile readFile = new ReadFile();
+
+            // if its file run as file
+            // otherwise run though files in directory
+            if (!file.isDirectory()) {
+
+                String startPath = filePath.getStartPath();
+                // make global so can write to file
+
+                // set path for login
+                readFile.setPath(startPath);
+                // pass inputs
+                String newStringFile = readFile.getNewFile(inputSearchFor, inputStringReplaceTo, isLine);
+
+                WriteToSource writeToSource = new WriteToSource(newStringFile, startPath);
+                writeToSource.writeFiles();
+
+                // show user what path was changed
+                inputFileText.setText(startPath);
+
+            } else {
+                filePath.setPaths();
+
+                for (String path : filePath.getPaths()) {
+
+                    // returns true if path is excluded
+                    ExcludePath excludePath = new ExcludePath(path, inputStringExclude.getText());
+                    if (excludePath.isExcluded()) {
+                        continue;
+                    }
+
+                    // set path for login
+                    readFile.setPath(path);
+                    // pass inputs
+                    String newStringFile = readFile.getNewFile(inputSearchFor, inputStringReplaceTo, isLine);
+
+                    WriteToSource writeToSource = new WriteToSource(newStringFile, path);
+                    writeToSource.writeFiles();
+
+                    // show user what path was changed ***Needs work***
+                    // inputFileText.setText(filePath.getPaths() + "\n");
+                }
+
+            }
+            this.inputSearchFor.setText("");
+            this.inputStringReplaceTo.setText("");
+            this.inputStringExclude.setText("");
+
+        } else {
+            System.out.println("Could not open file");
+        }
+    }
+}
--- src/main/java/com/karsonnichols/view/UserInterface.java
+++ src/main/java/com/karsonnichols/view/UserInterface.java
@@ -8,6 +8,7 @@ package com.karsonnichols.view;
  * not when they select path
  */
 
+import com.karsonnichols.controller.userInterfaceActions.FindProjectPath;
 import com.karsonnichols.model.ExcludePath;
 import com.karsonnichols.model.ReadFile;
 import com.karsonnichols.model.CreateFilePath;
@@ -135,7 +136,7 @@ public class UserInterface {
 
 
         // file button that starts the search
-        findFile.addActionListener(new FindProjectPath());
+        findFile.addActionListener(new FindProjectPath(this.mainFrame, this.inputSearchFor, this.inputStringReplaceTo, this.inputStringExclude, this.inputFileText, this.isLine));
 
         this.mainPanel.add(BorderLayout.CENTER, nested);
         this.mainFrame.getContentPane().add(BorderLayout.CENTER, mainPanel);
@@ -143,86 +144,6 @@ public class UserInterface {
         this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.mainFrame.setSize(1280, 780);
         this.mainFrame.setVisible(true);
-
-    }
-
-    // look in to implementing outside instead of inner class
-    public class FindProjectPath implements ActionListener {
-
-        public void actionPerformed(ActionEvent event){
-
-            // opens file window
-            JFileChooser fileChooser = new JFileChooser();
-            // allows for files and directories to be selected
-            fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            int option = fileChooser.showOpenDialog(mainFrame);
-
-            // check to make sure right action was preformed
-            if(option == JFileChooser.APPROVE_OPTION){
-                // String to hold file data
-                String afterLineRead = "";
-
-                // gets file and turns it to string path
-                File file = fileChooser.getSelectedFile();
-                String userPathToFile = file.getAbsolutePath();
-
-                // set the path
-                CreateFilePath filePath = new CreateFilePath(userPathToFile, inputStringExclude.getText());
-
-                ReadFile readFile = new ReadFile();
-
-                // if its file run as file
-                // otherwise run though files in directory
-                if(!file.isDirectory()){
-
-                    String startPath = filePath.getStartPath();
-                    // make global so can write to file
-
-                    // set path for login
-                    readFile.setPath(startPath);
-                    // pass inputs
-                    String newStringFile = readFile.getNewFile(inputSearchFor, inputStringReplaceTo, isLine);
-
-                    WriteToSource writeToSource = new WriteToSource(newStringFile, startPath);
-                    writeToSource.writeFiles();
-
-                    // show user what path was changed
-                    inputFileText.setText(startPath);
-
-                } else {
-                    filePath.setPaths();
-
-                    for (String path : filePath.getPaths()){
-
-                        // returns true if path is excluded
-                        ExcludePath excludePath = new ExcludePath(path, inputStringExclude.getText());
-                        if(excludePath.isExcluded()){
-                            continue;
-                        }
-
-                        // set path for login
-                        readFile.setPath(path);
-                        // pass inputs
-                        String newStringFile = readFile.getNewFile(inputSearchFor, inputStringReplaceTo, isLine);
-
-                        WriteToSource writeToSource = new WriteToSource(newStringFile, path);
-                        writeToSource.writeFiles();
-
-                        // show user what path was changed ***Needs work***
-                        // inputFileText.setText(filePath.getPaths() + "\n");
-                    }
-
-                }
-
-            }else{
-                System.out.println("Could not open file");
-            }
-
-            // clear input
-            inputSearchFor.setText("");
-            inputStringReplaceTo.setText("");
-            inputStringExclude.setText("");
-        } // end of action
 
     }
 
